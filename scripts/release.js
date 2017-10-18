076276d6d51d7a3767ffc2740d5c9f904cbbc20e
Oskar Hane <oh@oskarhane.com>
2017-10-18 12:01:44 +0200

    Add npm config files to dist folder
    Fixes publishing auth problems

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 
-const filesToCopy = ["package.json", "README.md", "LICENSE.txt"];
+const filesToCopy = [
+  "package.json",
+  "README.md",
+  "LICENSE.txt",
+  ".npmrc",
+  ".npmignore"
+];
 
 filesToCopy.forEach(file => fs.copyFileSync("./" + file, "dist/" + file));
