0c9a778c51b5d7c084a5103b128e336b721167d4
Oskar Hane <oh@oskarhane.com>
2017-11-09 20:39:09 +0100

    Dev server working good

--- src/dev/index.js
+++ src/dev/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Button } from "semantic-ui-react";
 
-const App = () => <p>Hello</p>;
+import { Chart } from "graph-app-kit/ui/Chart"; // This is aliased to the `src/` dir
+
+const App = () => [
+  <Chart
+    data={[{ label: "Used", value: 30 }, { label: "Free", value: 20 }]}
+    title="Static circular data"
+    chartType="doughnut"
+    type="json"
+  />,
+  <Button primary>Primary</Button>,
+  <Button secondary>Secondary</Button>
+];
 
 ReactDOM.render(<App />, document.getElementById("root"));
