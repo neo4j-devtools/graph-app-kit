be2f371253c9c8191565645508e3427fa1890ebd
Oskar Hane <oh@oskarhane.com>
2017-11-10 14:40:21 +0100

    Revert "Pull react and react-dom out of prod bundles"
    This reverts commit e5a281f44f20b1e070d0bdeee16a0f3e1c627672.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,8 +4,6 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const eslintFormatter = require("react-dev-utils/eslintFormatter");
 const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
 const paths = require("./paths");
 const getClientEnvironment = require("./env");
 
@@ -261,14 +259,8 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
-      reportFilename: "./../bundle-report.html"
-    })
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
-  externals: { react: "React", "react-dom": "ReactDOM" },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
