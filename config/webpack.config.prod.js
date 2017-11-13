48cb4d247603dc595a4f175f654049e28631719e
Oskar Hane <oh@oskarhane.com>
2017-11-13 09:08:23 +0100

    Add webpack-bundle-analyzer to analyze the bundle.
    It’s created on every `yarn build`.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,6 +4,8 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const eslintFormatter = require("react-dev-utils/eslintFormatter");
 const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 const paths = require("./paths");
 const getClientEnvironment = require("./env");
 
@@ -259,7 +261,12 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+      reportFilename: "./../bundle-report.html"
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
