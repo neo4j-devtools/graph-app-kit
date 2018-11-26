e998a0765fcf6a2025cc21cfd88bc8896b3ba72c
Jens Oknelid <jens.oknelid@gmail.com>
2018-11-26 15:55:13 +0100

    Make it possible to configure driver

--- src/components/GraphAppBase/helpers.js
+++ src/components/GraphAppBase/helpers.js
@@ -5,7 +5,7 @@ export const connectDriver = (
   driverFactory,
   onConnectionSuccessful,
   onConnectionFailure,
-  connectionTimeout = 5000
+  settings
 ) => {
   const { host, username, password, encrypted } = credentials;
   var driver = null;
@@ -14,7 +14,11 @@ export const connectDriver = (
       ? driverFactory.auth.basic(username, password)
       : undefined;
   try {
-    driver = driverFactory.driver(host, auth, { encrypted, connectionTimeout });
+    driver = driverFactory.driver(host, auth, {
+      connectionTimeout: 5000,
+      ...settings,
+      encrypted
+    });
   } catch (e) {
     onConnectionFailure(e);
     return;
